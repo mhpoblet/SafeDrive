8a13f4eabab20bdf92857b44071f5126262b682f
BishalT <35777544+BishalT@users.noreply.github.com>
2019-01-12 15:30:43 -0800

    init

---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var express = require('express'); //import express constructor
+var app = express(); // custom http framework
+var http = require('http').Server(app); //tell noejs to use the express framework instead of default framework
+
+
+
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+
+var fs = require('fs'); //filesystem
+var indexHtml = fs.readFileSync("./index.html");
+var testhtml = fs.readFileSync("./test.html");
+
+var accountSid = 'AC90f1457335a4bfa500cfff640e553a4d'; // Your Account SID from www.twilio.com/console
+var authToken = 'c950feb274876de4717a627d7ab531b2';
+var twilio = require('twilio');
+var client = new twilio(accountSid, authToken);
+
+client.messages.create({
+    body: 'foo',
+    to: '+16616755558',  // Text this number
+    from: '+16614413621' // From a valid Twilio number
+}).then((message) => console.log(message.sid));
+
+
+passport.use(new GoogleStrategy({
+  clientID: "1011794453706-3rhlsjcbqft61ijtdb6fqv9k2jhd0vvg.apps.googleusercontent.com",
+  clientSecret: "yDoxBLpAHwpq1mXotHx6ieM2",
+  callbackURL: "http://127.0.0.1/google/callback"
+},function(accessToken,refreshToken,profile,cb) {
+  return cb(null, profile);
+}));
+
+passport.serializeUser(function(user,done) {
+  done(null,user);
+}); //cb calls that
+
+passport.deserializeUser(function(user,done) {
+  done(null,user);
+}); //cb calls that
+
+
+//no matter request, use function will always get called first
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', function(request, response) {
+  console.log("this is root");
+  response.type("text/html");
+  response.send(indexHtml);
+});
+
+app.get('/google/login', passport.authenticate('google', {scope: ['profile']}));
+
+
+app.get('/google/callback', passport.authenticate('google', {failureRedirect: '/google/login'}), function(request, response) {
+
+  response.type("text/html"); //plain
+  response.send(testhtml);
+//response.send("Welcome  " + request.user.name.givenName +"!");
+  console.log(JSON.stringify(request.user));
+})
+
+app.get('/twilio/send', function(req,res){
+ console.log("sending message");
+});
+
+
+
+http.listen(80, function() {
+  console.log("starting server on port 80...");
+});
